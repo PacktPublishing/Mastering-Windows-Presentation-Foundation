cdfcb2a5099bcdb5c6e8b03a0aa47a9de799c8ba
packtpavanr <pavanr@packtpub.com>
2017-02-15 17:12:42 +0530

    Code files added

---
+++ CompanyName.ApplicationName/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using CompanyName.ApplicationName.ViewModels;
+
+namespace CompanyName.ApplicationName
+{
+    /// <summary>
+    /// The main display window for a WPF application
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initialises a new MainWindow object.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Closed += MainWindow_Closed;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWindowViewModel viewModel = new MainWindowViewModel();
+            viewModel.LoadSettings();
+            DataContext = viewModel;
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
+            viewModel.SaveSettings();
+        }
+    }
+}
